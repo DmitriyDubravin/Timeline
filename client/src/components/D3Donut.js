1da4365d0fcd2e04ea43b7912cc11d8b7510330e
DmitriyDubravin <dmitriy.dubravin@gmail.com>
2018-09-14 21:16:04 +0300

    d3 started

---
+++ client/src/components/D3Donut.js
@@ -0,0 +1,117 @@
+import React, {Component} from 'react';
+import * as d3 from "d3";
+
+console.log(d3);
+
+export default class Donut extends Component {
+    componentDidMount() {
+        var width = 960;
+        var height = 500;
+        var radius = Math.min(width, height) / 2;
+        var color = d3
+            .scaleOrdinal()
+            .range(["#98abc5", "#8a89a6", "#7b6888", "#6b486b", "#a05d56", "#d0743c", "#ff8c00"]);
+
+        var arc = d3
+            .arc()
+            .outerRadius(radius - 10)
+            .innerRadius(radius - 70);
+
+        var pie = d3
+            .pie()
+            .sort(null)
+            .value(function(d) { return d.population; });
+
+        var svg = d3
+            .select("#donut")
+            .append("svg")
+            .attr("width", width)
+            .attr("height", height)
+            .append("g")
+            .attr("transform", "translate(" + width / 2 + "," + height / 2 + ")");
+
+        d3
+            .csv("./../data/data.csv",
+            function(d) {
+                console.log(d);
+                d.population = +d.population;
+                return d;
+            },
+            function(error, data) {
+                // console.log(data);
+                if (error) throw error;
+
+                // var g = svg
+                //     .selectAll(".arc")
+                //     .data(pie(data))
+                //     .enter().append("g")
+                //     .attr("class", "arc");
+            
+            // g.append("path")
+            //     .attr("d", arc)
+            //     .style("fill", function(d) { return color(d.data.age); });
+            
+            // g.append("text")
+            //     .attr("transform", function(d) { return "translate(" + arc.centroid(d) + ")"; })
+            //     .attr("dy", ".35em")
+            //     .text(function(d) { return d.data.age; });
+        });
+    
+        function type(d) {
+            console.log(d);
+            d.population = +d.population;
+            return d;
+        }
+
+    }
+    render() {
+        return <div id="donut"></div>
+    }
+}
+
+
+/**
+ * var width = 960,
+    height = 500,
+    radius = Math.min(width, height) / 2;
+
+var color = d3.scale.ordinal()
+    .range(["#98abc5", "#8a89a6", "#7b6888", "#6b486b", "#a05d56", "#d0743c", "#ff8c00"]);
+
+var arc = d3.svg.arc()
+    .outerRadius(radius - 10)
+    .innerRadius(radius - 70);
+
+var pie = d3.layout.pie()
+    .sort(null)
+    .value(function(d) { return d.population; });
+
+var svg = d3.select("body").append("svg")
+    .attr("width", width)
+    .attr("height", height)
+  .append("g")
+    .attr("transform", "translate(" + width / 2 + "," + height / 2 + ")");
+
+d3.csv("data.csv", type, function(error, data) {
+  if (error) throw error;
+
+  var g = svg.selectAll(".arc")
+      .data(pie(data))
+    .enter().append("g")
+      .attr("class", "arc");
+
+  g.append("path")
+      .attr("d", arc)
+      .style("fill", function(d) { return color(d.data.age); });
+
+  g.append("text")
+      .attr("transform", function(d) { return "translate(" + arc.centroid(d) + ")"; })
+      .attr("dy", ".35em")
+      .text(function(d) { return d.data.age; });
+});
+
+function type(d) {
+  d.population = +d.population;
+  return d;
+}
+ */
--- client/src/pages/HomePage.js
+++ client/src/pages/HomePage.js
@@ -1,12 +1,18 @@
-import React, {Component} from 'react';
+import React, {Component, Fragment} from 'react';
 import {connect} from 'react-redux';
 import DefaultHomePage from './DefaultHomePage';
+import Donut from './../components/D3Donut';
 
 class HomePage extends Component {
     render() {
         if (!this.props.user.isAuthorized) return <DefaultHomePage />;
 
-        return <h2>Home page</h2>
+        return (
+            <Fragment>
+                <h2>Home page</h2>
+                <Donut />
+            </Fragment>
+        );
     }
 }
 
