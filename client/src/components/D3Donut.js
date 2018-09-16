3d30098210feecc504866e4d15e55823de22443d
DmitriyDubravin <dmitriy.dubravin@gmail.com>
2018-09-16 21:37:30 +0300

    d3 example integrated

--- client/src/components/D3Donut.js
+++ client/src/components/D3Donut.js
@@ -1,7 +1,6 @@
 import React, {Component} from 'react';
 import * as d3 from "d3";
-
-console.log(d3);
+import dataFile from './../data/data.csv';
 
 export default class Donut extends Component {
     componentDidMount() {
@@ -30,48 +29,27 @@ export default class Donut extends Component {
             .append("g")
             .attr("transform", "translate(" + width / 2 + "," + height / 2 + ")");
 
-        // d3.json("./data/data.json", function(one, two, three, four) {
-        //     console.log(0, one, two, three);
-        //     console.log(one);
-        // });
 
-        d3.csv("./../data/dat.csv").then(function(data) {
-            console.log(data); // [{"Hello": "world"}, …]
-        });
-        // d3.csv("./../data/data.csv", function(data) {
-        //     console.log(data[0]);
-        // });
-
-        var parsed = d3.csvParse("age,population\n<5,2704659\n5-13,4499890\n14-17,2159981\n18-24,3853788\n25-44,14106543\n45-64,8819342\n≥65,612463");
-        // console.log(parsed);
-
-        d3
-            .csv("./../data/data.csv",
-            function(d) {
-                // console.log(d);
-                d.population = +d.population;
-                return d;
-            },
-            function(data, index) {
-                // console.log(data);
-                // if (error) throw error;
-
+        d3.csv(dataFile).then(function(respArr) {
+            respArr.forEach(data => {
                 var g = svg
                     .selectAll(".arc")
-                    .data(pie(parsed))
-                    .enter().append("g")
+                    .data(pie(respArr))
+                    .enter()
+                    .append("g")
                     .attr("class", "arc");
-
-                g.append("path")
+                g
+                    .append("path")
                     .attr("d", arc)
                     .style("fill", function(d) {
                         return color(d.data.age);
                     });
-            
-                g.append("text")
+                g
+                    .append("text")
                     .attr("transform", function(d) { return "translate(" + arc.centroid(d) + ")"; })
                     .attr("dy", ".35em")
                     .text(function(d) { return d.data.age; });
+            })
         });
 
     }
@@ -79,50 +57,3 @@ export default class Donut extends Component {
         return <div id="donut"></div>
     }
 }
-
-
-/**
- * var width = 960,
-    height = 500,
-    radius = Math.min(width, height) / 2;
-
-var color = d3.scale.ordinal()
-    .range(["#98abc5", "#8a89a6", "#7b6888", "#6b486b", "#a05d56", "#d0743c", "#ff8c00"]);
-
-var arc = d3.svg.arc()
-    .outerRadius(radius - 10)
-    .innerRadius(radius - 70);
-
-var pie = d3.layout.pie()
-    .sort(null)
-    .value(function(d) { return d.population; });
-
-var svg = d3.select("body").append("svg")
-    .attr("width", width)
-    .attr("height", height)
-  .append("g")
-    .attr("transform", "translate(" + width / 2 + "," + height / 2 + ")");
-
-d3.csv("data.csv", type, function(error, data) {
-  if (error) throw error;
-
-  var g = svg.selectAll(".arc")
-      .data(pie(data))
-    .enter().append("g")
-      .attr("class", "arc");
-
-  g.append("path")
-      .attr("d", arc)
-      .style("fill", function(d) { return color(d.data.age); });
-
-  g.append("text")
-      .attr("transform", function(d) { return "translate(" + arc.centroid(d) + ")"; })
-      .attr("dy", ".35em")
-      .text(function(d) { return d.data.age; });
-});
-
-function type(d) {
-  d.population = +d.population;
-  return d;
-}
- */
