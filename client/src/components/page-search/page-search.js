670a5c433587ca2312f0eb79918c5424835b8be6
DmitriyDubravin <dmitriy.dubravin@gmail.com>
2019-02-05 22:21:43 +0200

    page search refactoring

--- client/src/components/page-search/page-search.js
+++ client/src/components/page-search/page-search.js
@@ -1,110 +1,128 @@
-import React, {Component} from 'react';
+import React, { useState, useEffect } from 'react';
 import {Link} from 'react-router-dom';
 import EventsList from './../../components/EventsList';
 import QM from './../../modules/QueryModule';
 import QS from 'query-string';
 import {extendEventWithHoursMinutes, removeEmptyKeys, checkEventModel} from './../../support/functions';
+import { Input, Button } from './../forms';
 
 
-// TODO: rebuild it
-class PageSearch extends Component {
-    constructor(props) {
-        super(props);
-        const queryString = props.location.search;
-        const queryObjTemplate = {type: '', category: '', subcategory: '', comment: ''};
-        const queryObj = {...queryObjTemplate, ...QS.parse(queryString)};
-        this.state = {queryObj, queryString};
-        this.inputHandler = this.inputHandler.bind(this);
-        this.editEvent = this.editEvent.bind(this);
-        this.deleteEvent = this.deleteEvent.bind(this);
-    }
 
-    componentDidMount() {
-        if (this.isSearchString()) {
-            this.search();
+const PageSearch = ({
+    location: { search },
+    name,
+    events,
+    ranges,
+    addRangeEvents,
+    openPopupEventEdit,
+    openPopupEventDelete
+}) => {
+
+    const [ type, setType ] = useState('');
+    const [ category, setCategory ] = useState('');
+    const [ subcategory, setSubcategory ] = useState('');
+    const [ comment, setComment ] = useState('');
+
+    const queryObjTemplate = {type, category, subcategory, comment};
+
+    const queryObj = {...queryObjTemplate, ...QS.parse(search)};
+    this.state = {queryObj, search};
+
+    useEffect(() => {
+        if (isSearchString()) {
+            search1();
         }
-    }
+    }, [search]);
 
-    componentDidUpdate(prevProps) {
-        if (prevProps.location.search !== this.props.location.search && this.isSearchString()) {
-            this.search();
-        }
-    }
-
-    async search() {
-        const {name, addRangeEvents} = this.props;
-        const {queryObj, queryString} = this.state;
+    async function search1() {
         const queryData = {
             author: name,
             queries: removeEmptyKeys(queryObj)
         };
-        const {success, eventsList} = await QM.search(queryString, queryData);
+        const {success, eventsList} = await QM.search(search, queryData);
         if (success) {
             const events = {};
             eventsList.forEach(event => {
                 events[event._id] = event;
             });
-            addRangeEvents(queryString, events);
+            addRangeEvents(search, events);
         }
     }
 
-    inputHandler(event) {
+    function submitHandler(event) {
         const {name, value} = event.target;
-        const queryObj = {...this.state.queryObj, [name]: value};
+    }
+
+    function inputHandler() {
+        // const queryObj = {queryObj, [name]: value};
         const string = QS.stringify(removeEmptyKeys(queryObj));
-        const queryString = string.length > 1 ? "?" + string : '';
-        this.setState({queryObj, queryString});
+        const s = string.length > 1 ? "?" + string : '';
+        this.setState({queryObj, s});
     }
 
-    isSearchString() {
-        return this.props.location.search.length > 0
+    function isSearchString() {
+        return search.length > 0
     }
 
-    editEvent(id) {
-        this.props.togglePopupEventEdit(id);
+    function editEvent(id) {
+        openPopupEventEdit(id);
     }
-    deleteEvent(id) {
-        this.props.togglePopupDeleteEvent(true, id);
+    function deleteEvent(id) {
+        openPopupEventDelete(id);
     }
 
 
 
-    render() {
+    const rangeIds = ranges[search];
+    const eventsList = rangeIds === undefined
+        ? []
+        : rangeIds.map(id => {
+            return extendEventWithHoursMinutes(checkEventModel(events[id]))
+        });
 
-        const {ranges, events} = this.props;
-        const {queryString} = this.state;
-        const rangeIds = ranges[queryString];
-        const eventsList = rangeIds === undefined
-            ? []
-            : rangeIds.map(id => {
-                return extendEventWithHoursMinutes(checkEventModel(events[id]))
-            });
-
-        return (
-            <div>
-                <h2>Search Page</h2>
-                <form className="search-form">
-                    <div className="line">
-                        <input value={this.state.queryObj.type} onChange={this.inputHandler} name="type" type="text" placeholder="Type" />
-                    </div>
-                    <div className="line">
-                        <input value={this.state.queryObj.category} onChange={this.inputHandler} name="category" type="text" placeholder="Category" />
-                    </div>
-                    <div className="line">
-                        <input value={this.state.queryObj.subcategory} onChange={this.inputHandler} name="subcategory" type="text" placeholder="Subcategory" />
-                    </div>
-                    <div className="line">
-                        <input value={this.state.queryObj.comment} onChange={this.inputHandler} name="comment" type="text" placeholder="Comment" />
-                    </div>
-                    <Link className="button" to={{
+    return (
+        <div>
+            <h2>Search Page</h2>
+            <form className="search-form" onSubmit={submitHandler}>
+                <div className="line">
+                    <Input
+                        placeholder="Type"
+                        value={type}
+                        onChange={setType}
+                    />
+                </div>
+                <div className="line">
+                    <Input
+                        placeholder="Category"
+                        value={category}
+                        onChange={setCategory}
+                    />
+                </div>
+                <div className="line">
+                    <Input
+                        placeholder="Subcategory"
+                        value={subcategory}
+                        onChange={setSubcategory}
+                    />
+                </div>
+                <div className="line">
+                    <Input
+                        placeholder="Comment"
+                        value={comment}
+                        onChange={setComment}
+                    />
+                </div>
+                <Button
+                    className="button"
+                    to={{
                         pathname: "/search",
-                        search: this.state.queryString
-                    }}>Search</Link>
-                </form>
-                <EventsList eventsListData={eventsList} editCb={this.editEvent} deleteCb={this.deleteEvent} />
-            </div>
-        );
-    }
+                        search: search
+                    }}
+                />
+            </form>
+            <EventsList eventsListData={eventsList} editCb={editEvent} deleteCb={deleteEvent} />
+        </div>
+    );
 }
 
 export default PageSearch;
