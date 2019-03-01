4aa995b4b923c9e938730e9aefd8a65eb42cb500
DmitriyDubravin <dmitriy.dubravin@gmail.com>
2019-03-01 21:50:17 +0200

    compose promises tests

--- server/src/get-events-list.js
+++ server/src/get-events-list.js
@@ -1,7 +1,31 @@
 const f = require('./support/functions');
 const e = require('./support/errors');
 
+const composePromise = (...functions) =>
+    initialValue =>
+        functions.reduceRight(
+            (sum, fn) => Promise.resolve(sum).then(fn),
+            initialValue
+    );
+
+const tryCatch = promise => {
+    console.log('sss', promise);
+    return promise.then(data => ({data}))
+    .catch(err => ({err}));
+}
+
+const tC = data => Promise.resolve(data)
+    .then(data => ({data}))
+    .catch(err => ({err}));
+
+const log = data => {
+    console.log('log:', data);
+    return data;
+}
+
 module.exports = async function(req, res) {
+
+    
 
     console.log('\n\n\nGET EVENTS LIST QUERY\n\n\n');
 
@@ -12,8 +36,16 @@ module.exports = async function(req, res) {
         finish: {$lte: finish}
     }
 
-    const {data, err} = await f.tryCatch(f.findEvents(findEventsOptions));
-    err && e.findEventsError(res);
+    const {data, err } = await composePromise(
+        tC,
+        f.findEvents,
+    )(findEventsOptions);
+
+    console.log(data);
+
+
+    // const {data, err} = await f.tryCatch(f.findEvents(findEventsOptions));
+    // err && e.findEventsError(res);
 
     f.success(res, {eventsList: data});
 
--- server/src/user-create.js
+++ server/src/user-create.js
@@ -12,33 +12,33 @@ module.exports = async function(req, res) {
 
     f.success(res);
 
-    // const foundUser = await f.tryCatch(f.findUser(findUserNameOptions));
-    // foundUser.err && e.findUserNameError(res);
+    const foundUser = await f.tryCatch(f.findUser(findUserNameOptions));
+    foundUser.err && e.findUserNameError(res);
 
-    // if (!f.isUserFound(foundUser.data)) {
+    if (!f.isUserFound(foundUser.data)) {
 
-    //     const hashedPassword = f.hashPassword(password);
-    //     const verificationHash = f.generateVerificationHash(login + password);
+        const hashedPassword = f.hashPassword(password);
+        const verificationHash = f.generateVerificationHash(login + password);
 
-    //     const addUserOptions = {
-    //         name: login,
-    //         password: hashedPassword,
-    //         email: email,
-    //         token: "no token",
-    //         role: verificationHash
-    //     };
+        const addUserOptions = {
+            name: login,
+            password: hashedPassword,
+            email: email,
+            token: "no token",
+            role: verificationHash
+        };
 
-    //     const addedUser = await f.tryCatch(f.addUser(addUserOptions));
-    //     addedUser.err && e.addUserError(res);
+        const addedUser = await f.tryCatch(f.addUser(addUserOptions));
+        addedUser.err && e.addUserError(res);
 
-    //     // SEND EMAIL
-    //     sendEmail(email, verificationHash);
+        // SEND EMAIL
+        sendEmail(email, verificationHash);
 
-    //     f.success(res);
+        f.success(res);
 
-    // } else {
+    } else {
 
-    //     f.failure(res);
+        f.failure(res);
 
-    // }
+    }
 }
